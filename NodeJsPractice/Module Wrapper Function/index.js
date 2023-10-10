727d4dd30ed2b86ac9f2839f9b76fc70a4c93ea1
Satyajit Mahakud <satyajit.mahakud@arithaconsulting.com>
2023-10-10 18:49:47 +0530

    Create and listen HTTP server on localhost

---
+++ NodeJsPractice/Module Wrapper Function/index.js
@@ -0,0 +1,7 @@
+(function (exports, require, module) {
+  const name = "satya";
+  console.log("My name is", name);
+  console.log("My file is", __filename);
+  console.log("My dir is", __dirname);
+})();
+//add one more parenthesis() at the end of anonomous function
